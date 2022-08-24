5e0720fd5149923bdefcc62e3da5ee53879d1969
Dmitriy Balmashnov <dmitri.balmashnov@gmail.com>
2022-08-24 13:17:51 +0200

    added queue system stub, created cards

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,7 @@
 console.log("Hello World!");
 
+
+
 class TimeSelector {
     constructor(root) {
         this.root = root
@@ -14,23 +16,34 @@ class TimeSelector {
         return container_tag
     }
 
+    #add(element) {
+        this.time_container.appendChild(element)
+        this.children.push(element)
+    }
+
     #createTimeElement(text) {
-        let time_element = document.createElement("li")
+        let time_element = document.createElement("button")
         time_element.setAttribute("class", "time-select-item")
+        time_element.textContent = text
 
         return time_element
     }
 
     setData(data) {
         this.clear()
-        for(time_ms of data) {
-            date = new Date(time_ms)
-            element = this.#createTimeElement(date.getLocaleTime())
+        for(let time_ms of data) {
+            let date = new Date(time_ms)
+            let element = this.#createTimeElement(date.toLocaleTimeString())
+            element.addEventListener('click', (e) => {
+                set_selected_timeslot(time_ms)
+                e.stopPropagation()
+            })
+            this.#add(element)
         }
     }
 
     clear() {
-        for(child of this.children) {
+        for(let child of this.children) {
             this.time_container.removeChild(child)
         }
         this.children = []
@@ -40,30 +53,39 @@ class TimeSelector {
 }
 
 class DateSelector {
-    constructor(root, index) {
+    constructor(root, index, days) {
         this.root = root;
-        [this.date_container, this.text_tag] = this.#createContainer(index)
+        [this.date_container, this.text_tag] = this.#createContainer(index, days)
         this.root.appendChild(this.date_container)
         this.time_selector = new TimeSelector(this.date_container)
+        this.is_active = false;
     }
 
-    #createContainer(index) {
+    #createContainer(index, days) {
         let container_tag = document.createElement("div")
         container_tag.setAttribute('id', "container-"+index.toString())
         container_tag.setAttribute(`class`, "day-selector")
 
-        let radio_tag = document.createElement("input")
-        radio_tag.setAttribute("type", "radio")
-        radio_tag.setAttribute("name", "day-selected")
-        radio_tag.setAttribute('class', "day-radio")
-        radio_tag.setAttribute("id", "radio-day-"+index.toString())
-        radio_tag.setAttribute("value", index.toString())
-
-        let text_tag = document.createElement("label")
-        text_tag.setAttribute("for", "radio-day-"+index.toString())
+        let text_tag = document.createElement("p")
         text_tag.textContent = ""
 
-        container_tag.appendChild(radio_tag)
+        container_tag.addEventListener("click", (event) => {
+            if(this.is_active) {
+                days.select(this)
+                event.stopPropagation()
+            }
+        })
+        container_tag.addEventListener("mouseenter", () => {
+            if(this.is_active) {
+                days.hoverStart(this)
+            }
+        })
+        container_tag.addEventListener("mouseleave", () => {
+            if(this.is_active) {
+                days.hoverEnd(this)
+            }
+        })
+
         container_tag.appendChild(text_tag)
 
         return [container_tag, text_tag]
@@ -73,6 +95,7 @@ class DateSelector {
         this.date_container.classList.add("day-enabled")
         this.date_container.classList.remove("day-disabled")
         this.text_tag.textContent = day
+        this.is_active = true;
         this.time_selector.setData(data)
     }
 
@@ -80,16 +103,65 @@ class DateSelector {
         this.date_container.classList.add("day-disabled")
         this.date_container.classList.remove("day-enabled")
         this.text_tag.textContent = day
+        this.is_active = false;
         this.time_selector.setData([])
     }
 
+    select() {
+        console.log("selected")
+        this.date_container.classList.add("day-selected")
+    }
+
+    unselect() {
+        console.log("unselected")
+        this.date_container.classList.remove("day-selected")
+    }
+}
+
+class Days {
+    constructor() {
+        this.current_selection = null
+    }
+
+    select(selector) {
+        if(this.current_selection === selector) {
+            this.current_selection.unselect()
+            this.current_selection = null
+            return
+        }
+        if(this.current_selection !== null) {
+            this.current_selection.unselect()
+        }
+        this.current_selection = selector
+        this.current_selection.select()
+    }
+
+    unselect() {
+        if(this.current_selection) {
+            this.current_selection.unselect()
+            this.current_selection = null
+        }
+    }
+
+    hoverStart(selector) {
+        if(this.current_selection === null) {
+            selector.select()
+        }
+    }
+
+    hoverEnd(selector) {
+        if(this.current_selection === null) {
+            selector.unselect()
+        }
+    }
 }
 
 class Calendar {
     constructor(root) {
         this.root_element = root;
         this.month_tag = document.getElementById("month")
-        this.sub_elements = this.#addElements(root);
+        this.days = new Days();
+        this.sub_elements = this.#addElements(root, this.days);
         var now = new Date(Date.now());
         this.currentTime = now;
         this.selectedMonth = this.currentTime;
@@ -97,10 +169,14 @@ class Calendar {
         this.setMonthTag(this.currentTime);
     }
 
-    #addElements(root) {
+    #addElements(root, days) {
+        this.root_element.addEventListener("click", (event)=> {
+            days.unselect()
+            event.stopPropagation()
+        })
         var new_elements = [];
         for(let i =0; i < (7*6); i++) {
-            let day_selector = new DateSelector(root, i)
+            let day_selector = new DateSelector(root, i, days)
             new_elements.push(day_selector)
         }
         return new_elements;
@@ -123,9 +199,9 @@ class Calendar {
     }
 
     flush() {
-        this.sub_elements.forEach( (e) => {
-        e.textContent = "";
-        });
+        for(let element of this.sub_elements) {
+            element.setDisabled("")
+        }
     }
 
     //takes a DateObject of current month and fills the elements
@@ -137,6 +213,9 @@ class Calendar {
         let total_fill = new Date(month.getYear(), month.getMonth()+1, 0).getDate();
         for (let i = 0; i < (total_fill); i++) {
             to_update[start_idx + i].setDisabled(i);
+            if(i % 2 == 0) {
+                to_update[start_idx + i].setEnabled(i,[ 5000, 30000, 50000 ])
+            }
         }
     }
 
@@ -145,10 +224,35 @@ class Calendar {
     }
 }
 
-calendar = new Calendar(document.getElementById("calendar"));
+class QueuePerson {
+    constructor(root) {
+        this.root = root;
 
-function overlay_on() {
+    }
+}
+
+class Queue {
+    constructor(root) {
+        this.root =
+        this.active_people = []
+        this.inactive_people = []
+        this.queue_active = false
+    }
+}
+
+function set_selected_timeslot(time_ms) {
+    console.log(`timeslot selected: ${time_ms}`)
+    //timeslot = document.getElementById("timeslot")
+    //timeslot.setAttribute('value', time_ms)
+}
+    
+calendar = new Calendar(document.getElementById("calendar"));
+let about_active = ""
+
+function overlay_on(target_about) {
     document.getElementById("overlay").style.display = "flex";
+    document.getElementById(target_about).style.display = "flex";
+    about_active = target_about
     document.body.classList.add("noscroll");
     document.getElementById("body").classList.add("blur")
     fillCalendar();
@@ -157,9 +261,10 @@ function overlay_on() {
 document.getElementById("overlay").addEventListener("click", function( e ){
     e = window.event || e; 
     if(this === e.target) {
-    document.getElementById("overlay").style.display = "none";
-    document.body.classList.remove("noscroll")
-    document.getElementById("body").classList.remove("blur")
+        document.getElementById("overlay").style.display = "none";
+        document.getElementById(about_active).style.display = "none"
+        document.body.classList.remove("noscroll")
+        document.getElementById("body").classList.remove("blur")
     }
 });
 
@@ -169,8 +274,4 @@ function next_month() {
 
 function prev_month() {
     calendar.prev_month();
-}
-
-function fillCalendar() {
-
 }
